dc32265e70985a479ca57cbe0bbba0f9dcd244cc
Darragh Conneely <darraghc23@gmail.com>
2023-05-26 21:47:04 +0100

    updated api base url to render site

--- src/services/camperpins-service.js
+++ src/services/camperpins-service.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { user } from "../stores";
 
 export const camperpinsService = {
-    baseUrl: "http://localhost:9128",
+    baseUrl: "https://camperpins-2.onrender.com/",
 
     async login(email, password) {
         try {
